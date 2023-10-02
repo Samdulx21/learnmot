620785ac6052fba7be95cdfadd81d571c02ede69
millow <millow@github.com>
2023-10-02 17:31:36 -0500

    ejemplo con jinja2

--- app/routes/HomeRoute.py
+++ app/routes/HomeRoute.py
@@ -3,12 +3,12 @@ from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
-templates = Jinja2Templates(directory="../static/templates/layouts")
+templates = Jinja2Templates(directory="../static/templates")
 
 @router.get("/")
-async def root():
-    return {"msg": "Welcome to new project in FastApi. :D"}
+async def root(request: Request):
+    return templates.TemplateResponse("home.html",{"request": request})
 
-@router.get("/home")
-async def index(request: Request):
-    return templates.TemplateResponse("base.html",{"request": request})
+# @router.get("/example")
+# async def index(request: Request):
+#     return templates.TemplateResponse("base.html",{"request": request})
