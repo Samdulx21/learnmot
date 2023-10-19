8763b9eeaeb69f04c9e027d5c913eaa4a320c379
millow <millow@github.com>
2023-10-19 18:30:15 -0500

    Components Add

---
+++ motcrud/src/pages/users/UsersHome.jsx
@@ -0,0 +1,17 @@
+import SideMenu from "../components/sidemenu/sideMenu"
+
+function UsersHome(){
+    return (
+        <>
+            <div >
+                 <SideMenu /> 
+                    <div className="">
+
+
+                    </div>             
+            </div>
+        </>
+    )
+}
+
+export default UsersHome
